6d9ace9cacc16e28f3ead4f0d406fb5c231352bd
newland <juanantonio_calero@hotmail.com>
2017-07-10 20:00:10 +0200

    Formateo con WebStorm

--- WebApplication4/app/app.component.ts
+++ WebApplication4/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `
+    selector: 'my-app',
+    template: `
 <h1>DS {{title}}</h1>
 <app-header></app-header>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+    Left Component
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
+    Right Component
+</div>
+</div>
 <app-footer></app-footer>
 `,
 })
-export class AppComponent  {
+export class AppComponent {
 }
